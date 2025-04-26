470c7256eac221affc97ff9820d4c3df16c9e4eb
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2025-04-26 08:19:03 +0500

    added one more controller

--- Booking App/routes/hotels.js
+++ Booking App/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
 import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
@@ -14,9 +14,12 @@ router.put('/:id', verifyAdmin, updateHotel)
 router.delete('/:id', verifyAdmin, deleteHotel)
 
 // get
-router.get('/:id', getHotel)
+router.get('/find/:id', getHotel)
 
 //get all
 router.get('/', getHotels)
+router.get('/countByCity', countByCity)
+router.get('/countByType', getHotels)
+
 
 export default router
